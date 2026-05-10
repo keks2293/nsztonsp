4f7da6406fdd621018deed950affefb99fe3a395
keks2293 <rusik2293@gmail.com>
2026-05-10 06:37:19 +0300

    Remove dead node/ files (keys.js, parseArguments.js, pathTools.js) and update docs

--- node/pathTools.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export function changeExtension(filePath, newExtension) {
-    const parsed = path.parse(filePath);
-    return path.join(parsed.dir, parsed.name + newExtension);
-}
